e024aff1eae6bea780b5cd976440841c7fd1ea08
Ginevrana <florenciateresita@gmail.com>
2022-11-21 18:39:57 -0300

    Ejercicio 5 hecho

---
+++ ejercicios/Sanchez_DSP5.py
@@ -0,0 +1,5 @@
+horas = int(input("¿Cuántas horas trabajó? "))
+costo = int(input("¿Qué costo tiene su hora de trabajo? "))
+sueldo = horas * costo
+
+print(f"Según los datos ingresados le corresponde ${sueldo} de sueldo")
